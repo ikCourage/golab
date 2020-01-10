a9d8ca88fdfb6e252bd0b40eb77f1ce5add565e1
ikCourage <ikcourage>
2020-01-10 10:56:23 +0800

    init

---
+++ gotrans/gotrans.go
@@ -0,0 +1,297 @@
+package gotrans
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+type tField struct {
+	FromKey   string
+	ToKey     string
+	Index     int
+	IndexNode int
+	IndexArr  []int
+	Type      reflect.Type
+	Kind      reflect.Kind
+}
+
+type typeDesc struct {
+	Write map[string]*tField
+	Read  []tField
+}
+
+type TransFunc func(from, to, node, root unsafe.Pointer) error
+
+var (
+	globalTypeDescMap  = make(map[reflect.Type]*typeDesc)
+	globalTransFuncMap = make(map[string]TransFunc)
+)
+
+func getKeyByField(fd *reflect.StructField) (string, string, string) {
+	key := fd.Name
+	if 'A' <= key[0] && key[0] <= 'Z' {
+		if tag, ok := fd.Tag.Lookup("trans2"); ok {
+			l := len(tag)
+			for i := 0; i < l; i++ {
+				if tag[i] == ':' {
+					return "", tag[:i], tag[i+1:]
+				}
+			}
+			return "", "-", ""
+		}
+		if tag, ok := fd.Tag.Lookup("trans"); ok {
+			if tag != "" {
+				return tag, "", ""
+			}
+			return key, "", ""
+		}
+	}
+	return "", "", ""
+}
+
+func isInvalidType(tt reflect.Type) (bool, error) {
+__loop:
+	switch tt.Kind() {
+	case reflect.Struct:
+		if _, err := getTypeDesc(tt); nil != err {
+			return false, err
+		}
+	case reflect.Map:
+		// 检查 map 的 key 是否为基本类型
+		if tt.Key().Kind() != reflect.String {
+			return true, nil
+		}
+		fallthrough
+	case reflect.Slice, reflect.Array, reflect.Ptr:
+		tt = tt.Elem()
+		goto __loop
+	}
+	return false, nil
+}
+
+func (tdesc *typeDesc) walkStruct(rt reflect.Type) (err error) {
+	count := 0
+	l := rt.NumField()
+	tdesc.Read = make([]tField, 0, l)
+	tdesc.Write = make(map[string]*tField, l>>1)
+	var tdesc2 *typeDesc
+	var tfd *tField
+	var key string
+	var fromKey string
+	var toKey string
+	var invalid bool
+	var fd reflect.StructField
+	var kind reflect.Kind
+	for i := 0; i < l; i++ {
+		fd = rt.Field(i)
+		if fd.Anonymous {
+			tdesc2, err = getTypeDesc(fd.Type)
+			if nil != err {
+				return err
+			}
+			arr2 := tdesc2.Read
+			l2 := len(arr2)
+			if l2 != 0 {
+				indexArr2 := []int{i}
+				for i2 := 0; i2 < l2; i2++ {
+					tdesc.Read = append(tdesc.Read, arr2[i2])
+					tfd = &tdesc.Read[count]
+					count++
+					if nil != tfd.IndexArr {
+						tfd.IndexArr = append(indexArr2, tfd.IndexArr...)
+					} else {
+						tfd.IndexArr = append(indexArr2, tfd.Index)
+						tfd.IndexNode = i
+					}
+				}
+				for k, v := range tdesc2.Write {
+					if _, ok := tdesc.Write[k]; ok {
+						return fmt.Errorf("duplicate key: %s at %v", k, v.Type)
+					}
+					tfd = &tField{
+						Index: v.Index,
+						Type:  v.Type,
+					}
+					if nil != v.IndexArr {
+						tfd.IndexArr = append(indexArr2, v.IndexArr...)
+					} else {
+						tfd.IndexArr = append(indexArr2, v.Index)
+					}
+					tdesc.Write[k] = tfd
+				}
+			}
+		} else {
+			key, fromKey, toKey = getKeyByField(&fd)
+			if fromKey != "" {
+				kind = fd.Type.Kind()
+				switch kind {
+				case reflect.Struct:
+					_, err = getTypeDesc(fd.Type)
+					if nil != err {
+						return
+					}
+				case reflect.Slice, reflect.Array, reflect.Ptr:
+					invalid, err = isInvalidType(fd.Type.Elem())
+					if nil != err {
+						return
+					}
+					if invalid {
+						continue
+					}
+				case reflect.Map:
+					invalid, err = isInvalidType(fd.Type)
+					if nil != err {
+						return
+					}
+					if invalid {
+						continue
+					}
+				case reflect.Func, reflect.Chan, reflect.UnsafePointer:
+					continue
+				}
+				tdesc.Read = append(tdesc.Read, tField{
+					FromKey: fromKey,
+					ToKey:   toKey,
+					Index:   i,
+					Type:    fd.Type,
+					Kind:    kind,
+				})
+				count++
+			} else if key != "" {
+				if _, ok := tdesc.Write[key]; ok {
+					return fmt.Errorf("duplicate key: %s at %v", key, rt)
+				}
+				tdesc.Write[key] = &tField{
+					Index: i,
+					Type:  fd.Type,
+				}
+			}
+		}
+	}
+	for count > 0 {
+		count--
+		tfd = &tdesc.Read[count]
+		if tfd.ToKey != "" {
+			if _, ok := globalTransFuncMap[tfd.FromKey]; !ok {
+				return fmt.Errorf("function %s not found, at %v", tfd.FromKey, tfd.Type)
+			}
+			if _, ok := tdesc.Write[tfd.ToKey]; !ok {
+				return fmt.Errorf("field %s not found, at %v", tfd.ToKey, tfd.Type)
+			}
+		}
+	}
+	return
+}
+
+func getTypeDesc(rt reflect.Type) (*typeDesc, error) {
+	if tdesc, ok := globalTypeDescMap[rt]; ok {
+		return tdesc, nil
+	}
+	tdesc := &typeDesc{}
+	if err := tdesc.walkStruct(rt); nil != err {
+		return nil, err
+	}
+	globalTypeDescMap[rt] = tdesc
+	return tdesc, nil
+}
+
+func RegisterTransFunc(name string, f TransFunc) {
+	if _, ok := globalTransFuncMap[name]; ok {
+		panic(fmt.Errorf("duplicate name %s", name))
+	}
+	globalTransFuncMap[name] = f
+}
+
+func transValue(rv reflect.Value, rt reflect.Type, rk reflect.Kind, root unsafe.Pointer) (err error) {
+	switch rk {
+	case reflect.Struct:
+		tdesc, err := getTypeDesc(rt)
+		if nil != err {
+			return err
+		}
+		var tfd *tField
+		var vv, vv2 reflect.Value
+		var node unsafe.Pointer
+		nodeV := unsafe.Pointer(rv.UnsafeAddr())
+		l := len(tdesc.Read)
+		for i := 0; i < l; i++ {
+			tfd = &tdesc.Read[i]
+			if tfd.FromKey != "" {
+				if nil != tfd.IndexArr {
+					node = unsafe.Pointer(rv.Field(tfd.IndexNode).UnsafeAddr())
+					vv = rv.FieldByIndex(tfd.IndexArr)
+				} else {
+					node = nodeV
+					vv = rv.Field(tfd.Index)
+				}
+				if tfd.ToKey != "" {
+					tfunc := globalTransFuncMap[tfd.FromKey]
+					tfd = tdesc.Write[tfd.ToKey]
+					if nil != tfd.IndexArr {
+						vv2 = rv.FieldByIndex(tfd.IndexArr)
+					} else {
+						vv2 = rv.Field(tfd.Index)
+					}
+					err = tfunc(unsafe.Pointer(vv.UnsafeAddr()), unsafe.Pointer(vv2.UnsafeAddr()), node, root)
+				} else {
+					err = transValue(vv, tfd.Type, tfd.Kind, root)
+				}
+				if nil != err {
+					return err
+				}
+			}
+		}
+	case reflect.Slice:
+		if rv.IsNil() {
+			return
+		}
+		fallthrough
+	case reflect.Array:
+		rt = rt.Elem()
+		rk = rt.Kind()
+		l := rv.Len()
+		for i := 0; i < l; i++ {
+			err = transValue(rv.Index(i), rt, rk, root)
+			if nil != err {
+				return
+			}
+		}
+	case reflect.Map:
+		if rv.IsNil() {
+			return
+		}
+		for _, k := range rv.MapKeys() {
+			err = transValue(rv.MapIndex(k), rt, rk, root)
+			if nil != err {
+				return
+			}
+		}
+	case reflect.Ptr, reflect.Interface:
+		if rv.IsNil() {
+			return
+		}
+		rv = rv.Elem()
+		rt = rv.Type()
+		err = transValue(rv, rt, rt.Kind(), root)
+		if nil != err {
+			return
+		}
+	}
+	return nil
+}
+
+func Trans(v interface{}) error {
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr {
+		return fmt.Errorf("not pointer")
+	}
+__loop:
+	rv = rv.Elem()
+	if rv.Kind() == reflect.Ptr {
+		goto __loop
+	}
+	rt := rv.Type()
+	rk := rt.Kind()
+	return transValue(rv, rt, rk, unsafe.Pointer(rv.UnsafeAddr()))
+}
---
+++ snowflake/snowflake.go
@@ -0,0 +1,65 @@
+package snowflake
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type snowFlake struct {
+	lk            sync.Mutex
+	epoch         int64
+	lastTimestamp int64
+	workerId      int64
+	sequence      int64
+}
+
+type SnowFlake interface {
+	NextId() (int64, error)
+}
+
+type Config struct {
+	Epoch    int64
+	WorkerId int64
+}
+
+var (
+	defaultConfig = &Config{
+		Epoch: 1578499200000,
+	}
+)
+
+func New(config *Config) SnowFlake {
+	if nil == config {
+		config = defaultConfig
+	}
+	return &snowFlake{
+		epoch:    config.Epoch,
+		workerId: config.WorkerId,
+	}
+}
+
+func (self *snowFlake) NextId() (int64, error) {
+	self.lk.Lock()
+	timestamp := time.Now().UnixNano() / 1e6
+	if timestamp < self.lastTimestamp {
+		self.lk.Unlock()
+		return 0, fmt.Errorf("clock is back: %d from previous: %d", timestamp, self.lastTimestamp)
+		timestamp = self.lastTimestamp
+	}
+	if timestamp == self.lastTimestamp {
+		self.sequence = (self.sequence + 1) & 4095
+		if self.sequence == 0 {
+			// 借一毫秒
+			timestamp++
+		}
+	} else {
+		self.sequence = 0
+	}
+	self.lastTimestamp = timestamp
+	self.lk.Unlock()
+	id := ((timestamp - self.epoch) << 22) |
+		(self.workerId << 12) |
+		self.sequence
+	return id, nil
+}
